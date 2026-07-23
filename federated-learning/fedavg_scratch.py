c776e8cc5c9ff1bacb069f9f58c32f4922d93540
Harshaa <158066517+harshaa77j@users.noreply.github.com>
2026-07-23 08:34:18 +0530

    Removed fedavg, to be implemented later

--- federated-learning/fedavg_scratch.py
+++
@@ -1,7 +0,0 @@
-# what i'm trying to show:
-# if two people each study half a textbook,
-# and share what they learned,
-# together they know more than either one alone.
-# that's federated learning.
-# let me prove it works.
-
